14dbe2071108f9cb0bce98a88168bb484cc4a6a7
Quân Intel <quankieu99@gmail.com>
2024-08-30 01:32:51 +0700

    Clean Trash

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,45 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
+import { Col, Container, Row, Table } from 'react-bootstrap';
+import './app.scss';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="view-users-oauth2" />
-    </div>
+    <Container className="container">
+      <Row>
+        
+      </Row>
+      <Row>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Row>
+    </Container>
   );
 }
 
